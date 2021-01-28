921cf39ed5c6d6f4f1289d1f814888ce72bb7203
Brian Douglass <bhdouglass@gmail.com>
2021-01-28 15:41:24 -0500

    Added gitlab ci setup

--- ci/16.04/build.sh
+++ ci/16.04/build.sh
@@ -2,5 +2,8 @@
 
 TARGET_ARCH=${TARGET_ARCH:-armhf}
 TAG=${TAG:-latest}
+TARGET_TAG=${TARGET_TAG:-latest}
 
-docker build -t clickable/ci-16.04-$TARGET_ARCH:$TAG --build-arg TARGET_ARCH=$TARGET_ARCH . $@
+docker build -t clickable/ci-16.04-$TARGET_ARCH:$TAG \
+    --build-arg TARGET_ARCH=$TARGET_ARCH \
+    .
